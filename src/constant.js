15016dd7864885bde06fb8fe8c15b50de39875f2
Ashok <ashok.ag.ganika@gmail.com>
2021-03-02 10:37:09 +0545

    upload

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+export const BankEmployeeDetails = [
+  { Id: 1, Name: "Nabil_Bank" },
+  { Id: 2, Name: "Nepal_Investment_Bank" },
+  { Id: 3, Name: "Global_IME" },
+  { Id: 4, Name: "NIC_ASIA" },
+  { Id: 5, Name: "PRABHU" },
+];
